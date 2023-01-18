afe2ee04bbc67b3ca9ce820a6fdc106e894895c8
Matthews Bastos <matthewsbasttos@gmail.com>
2023-01-18 19:04:53 -0300

    Implementa o método createdRacesInstances nas subclasses de Race

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,6 +1,8 @@
 import Race from './Race';
 
 export default class Halfling extends Race {
+  private static _numberOfInstances = 0;
+
   constructor(
     name: string,
     dexterity: number,
@@ -11,5 +13,10 @@ export default class Halfling extends Race {
 
   public get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    this._numberOfInstances += 1;
+    return this._numberOfInstances;
   }
 }
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,6 +1,4 @@
 export default abstract class Race {
-  private static _numberOfInstances = 0;
-
   constructor(
     private _name: string,
     private _dexterity: number,
@@ -17,11 +15,6 @@ export default abstract class Race {
   public abstract get maxLifePoints(): number;
 
   public static createdRacesInstances(): number {
-    try {
-      this._numberOfInstances += 1;
-      return this._numberOfInstances;
-    } catch {
-      throw new Error('Not implemented');
-    }
+    throw new Error('Not implemented');
   }
 }
