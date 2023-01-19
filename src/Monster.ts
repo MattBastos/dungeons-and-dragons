55d07f4fada68d1c9e2761ba68a713d270c0e0da
Matthews Bastos <matthewsbasttos@gmail.com>
2023-01-19 19:23:21 -0300

    Adiciona o arquivo index

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,13 +1,10 @@
 import { SimpleFighter } from './Fighter';
 
 export default class Monster implements SimpleFighter {
-  protected _lifePoints: number;
-  protected _strength: number;
-  
-  constructor() {
-    this._lifePoints = 85;
-    this._strength = 63;
-  }
+  constructor(
+    protected _lifePoints: number = 85,
+    protected _strength: number = 63,
+  ) { }
 
   public get lifePoints(): number {
     return this._lifePoints;
